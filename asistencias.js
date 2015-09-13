832a1f371693b474633a97ae22ab7cf3e17f2b4b
Juan Pablo Prado <juan@juanpabloprado.com>
2015-09-13 01:13:05 -0500

    cleaning object

--- asistencias.js
+++ asistencias.js
@@ -3,47 +3,40 @@ var cheerio = require('cheerio');
 var fs = require('fs');
 
    
-function  asistencias(idDip,id,dato){
+function  asistencias(idDip ,id ,json){
 
-
-	var datoDiputado={datosDiputado:dato,asistencia:{}};
 
 	url='http://sitl.diputados.gob.mx/LXII_leg/asistencias_por_pernplxii.php?iddipt='+idDip+'&pert='+id;
 	request(url, function(error, response, html){
+		var asistencias = [];
 		if(!error){
 			var $ = cheerio.load(html);
-			var meses={asistencias:[]};
-			var title, release, rating;
-			
-			var text='';
-
 
 			$("table [bgcolor='#F2F2F2']").each(function(i, elem) {
-				var json = { mes : "", dias :""};
+				var asistencia = { mes : "", dias :"" };
 				var titulo=$(this).find('.TitulosVerde').html();
-				if(titulo!=null){
+				if(titulo != null){
 					//console.log(titulo);
-					json.mes=titulo;	
+					asistencia.mes = titulo;
 					var contDias=0;
 					var jsonTemp=[];
 					$(this).find("td [bgcolor='#D6E2E2']").each(function(i, elem) {
 						var datos=$(this).find("font").html();
-						datos=datos.split("<br>");
+						datos = datos.split("<br>");
 						//console.log(datos);
-						var dia={dia:datos[0],concepto:datos[1]};
-						jsonTemp[contDias]=dia;
+						var dia = {dia: datos[0],concepto: datos[1]};
+						jsonTemp[contDias] = dia;
 						contDias++;
-						//text=text+datos;
 					});
-					json.dias=jsonTemp;
+					asistencia.dias=jsonTemp;
 				}
-				if(json.mes!=""){
-					meses.asistencias.push(json);	
+				if(asistencia.mes!=""){
+					asistencias.push(asistencia);
 				}
 				
 			});				        
 		}
-		datoDiputado.asistencia=meses;
+		json.asistencias = asistencias;
 	 //fs.writeFile('output.html', JSON.stringify(datoDiputado, null, 4), function(err){
       //  	console.log('File successfully written! - Check your project directory for the output.html file');
       //});
