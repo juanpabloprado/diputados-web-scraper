3cc2d8b924ac86047b5dd94737ddff3f48a17428
israel <israel.canul@gmail.com>
2015-09-12 23:17:07 -0500

    cambios

--- server.js
+++ server.js
@@ -3,41 +3,59 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
+var datosDiputado=require('./datosDiputado');
+
 
 app.get('/scrape', function(req, res){
 	// Let's scrape Anchorman 2
-	url = 'http://www.imdb.com/title/tt1229340/';
+	//url = 'http://www.imdb.com/title/tt1229340/';
+    datosDiputado(430);
+
+/*	url= 'http://sitl.diputados.gob.mx/LXII_leg/asistencias_por_pernplxii.php?iddipt=430&pert=11';
+	
 
 	request(url, function(error, response, html){
 		if(!error){
 			var $ = cheerio.load(html);
-
+			var meses={asistencias:[]};
 			var title, release, rating;
-			var json = { title : "", release : "", rating : ""};
+			
+			var text='';
 
-			$('.header').filter(function(){
-		        var data = $(this);
-		        title = data.children().first().text();
-		        release = data.children().last().children().text();
 
-		        json.title = title;
-		        json.release = release;
-	        });
+			$("table [bgcolor='#F2F2F2']").each(function(i, elem) {
+				var json = { mes : "", dias :""};
+				var titulo=$(this).find('.TitulosVerde').html();
+				if(titulo!=null){
+					console.log(titulo);
+					json.mes=titulo;	
+					var contDias=0;
+					var jsonTemp=[];
+					$(this).find("td [bgcolor='#D6E2E2']").each(function(i, elem) {
+						var datos=$(this).find("font").html();
+						datos=datos.split("<br>");
+						console.log(datos);
+						var dia={dia:datos[0],concepto:datos[1]};
+						jsonTemp[contDias]=dia;
+						contDias++;
+						//text=text+datos;
+					});
+					json.dias=jsonTemp;
+				}
+				if(json.mes!=""){
+					meses.asistencias.push(json);	
+				}
+				
+			});
+				        
+		}  
+     res.send('Check your console!'+meses)
 
-	        $('.star-box-giga-star').filter(function(){
-	        	var data = $(this);
-	        	rating = data.text();
+       
+	})*/
 
-	        	json.rating = rating;
-	        })
-		}
+ 
 
-		fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-        	console.log('File successfully written! - Check your project directory for the output.json file');
-        });
-
-        res.send('Check your console!')
-	})
 });
 
 app.listen('8081');
